4a484f35c67f67d71257179c0fa5b07bfc8262bd
printCelinaPabst <celina.pabst@gmx.net>
2025-07-15 23:15:01 +0200

    Add main_app.py as entry point for modular application

---
+++ main_app.py
@@ -0,0 +1,19 @@
+# main_app.py
+
+# TODO 5: Importiere die Funktion 'say_hello' aus dem Modul 'greetings'.
+from greetings import say_hello
+
+# TODO 6: Importiere die Funktion 'add' aus dem Modul 'math_utils'.
+from math_utils import add
+
+if __name__ == "__main__":
+    print("Willkommen zu deiner eigenen Modul-Anwendung!")
+
+    # TODO 7: Rufe say_hello mit deinem Namen auf und gib das Ergebnis aus.
+    say_hello()
+
+    # TODO 8: Rufe add mit zwei Zahlen auf und gib das Ergebnis aus.
+    #add(2, 6)
+    add()
+
+    #print("Bitte implementieren Sie die TODOs, um die App zu starten!")
